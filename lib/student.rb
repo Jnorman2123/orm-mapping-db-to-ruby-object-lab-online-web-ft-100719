378b146510ae69f426e8c3cd776b57a0afdae123
Jonathan Norman <jnorman2123@gmail.com>
2019-11-07 02:06:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,13 @@ class Student
   end
 
   def self.create_table
-
+    sql <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
